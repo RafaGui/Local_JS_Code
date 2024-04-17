bef3370c2456c5f197941bacd623d90abefc4d9e
RafaGui <rafaelwilliansgi4@gmail.com>
2024-04-17 19:03:08 -0300

    pokedex finalizada

--- Operador Spread/main.js
+++ Operador Spread/main.js
@@ -12,6 +12,8 @@ var user = {
     nome, 
     idade,
     ...empresa
+    // o operador spread é invocado com "..." e serve para 
+    //adiconar os valores de um array em outro como um ctrl c + ctrl v
 }
 
 console.log(user);
